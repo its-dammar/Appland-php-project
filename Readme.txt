2cc3bc4fb94c4918ff8c72bfed02d00fd086332d
dammar200 <dammarkhadayat.xdezo@gmail.com>
2023-07-16 18:29:41 +0545

    add database file and create database in readme.txt file

---
+++ Readme.txt
@@ -0,0 +1,43 @@
+
+
+users
+id | name | phone| address| email |  password | status | created_at | updated_at
+
+hero
+ id | title | description | img | status | created_at | updated_at
+
+ app_feature
+ id | top_title | description | img | status | created_at | updated_at
+
+ appfeatures
+ id | icon | title | description | status | created_at | updated_at
+ 
+ abouts
+ id | img | title | description | status | created_at | updated_at
+
+ services
+ id | title | status | created_at | updated_at
+
+ galleries
+ id | img | status | created_at | updated_at
+
+ Testimonials
+ id | img | name | post | message |  status | created_at | updated_at
+
+ pricing 
+ id | title | price | sub_title |  status | created_at | updated_at
+
+ price_features
+ id | title |  status | created_at | updated_at
+
+ faqs
+ id | qn | ans |  status | created_at | updated_at
+
+ message
+ id | name | email | subject | message |  status | created_at | updated_at
+
+news_letters
+id | email |  status | created_at | updated_at
+
+ settings
+ id | site_key | site_value |  status | created_at | updated_at
---
+++ users.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jul 16, 2023 at 12:38 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `appland`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `pasword` varchar(200) NOT NULL,
+  `status` int NOT NULL DEFAULT '1',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+
+DROP TABLE IF EXISTS `hero`;
+CREATE TABLE IF NOT EXISTS `hero` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(150) NOT NULL,
+  `description` varchar(15) NOT NULL,
+  `img` varchar(50) NOT NULL,
+  `status` int NOT NULL DEFAULT '1',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
